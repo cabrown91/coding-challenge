b2bd9628462a0b98ad446e5681db1511ae6978ca
Alicia Brown <cleo.aliciabrown@gmail.com>
2016-10-25 15:08:15 -0700

    add /api route + JSON data to document endpoints

--- server.js
+++ server.js
@@ -23,7 +23,23 @@ app.get('/', function homepage(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+/*
+ * JSON API Endpoints
+ */
 
+app.get('/api', function api_index(req, res) {
+  // TODO: Document all your api endpoints below
+  res.json({
+    message: "Welcome to my personal api! Here's what you need to know!",
+    documentationUrl: "https://github.com/cabrown91/coding-challenge",
+    // baseUrl: "https://stark-dawn-84023.herokuapp.com/",
+    endpoints: [
+      {method: "GET", path: "/api", description: "Describes all available endpoints"},
+      {method: "GET", path: "/api/talks", description: "Data about talks"},
+      {method: "GET", path: "/api/attendees", description: "Data about attendees"},
+    ]
+  });
+});
 
 
 
